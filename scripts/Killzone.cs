74030cb5ffdea7d20595db5b69b5c21aec2a7682
saitakasaine <saitakasaine@gmail.com>
2024-11-14 01:12:12 +0300

    OnTimerTimeOut

--- scripts/Killzone.cs
+++ scripts/Killzone.cs
@@ -3,10 +3,22 @@ using System;
 
 public partial class Killzone : Area2D
 {
-    private Timer _timer;
+    [Export] private Timer _timer;
     private void OnBodyEntered(Node2D body)
     {
+        
+        //_timer.GetTree().ReloadCurrentScene();
         GD.Print("You died");
+        _timer.Start();
+        _timer.Timeout += OnTimerTimeOut;
+    }
+    private void OnTimerTimeOut()
+    {   
+        //_limit.Timeout -= OnTimerTimeOut;
+        GD.Print("Game restarted");
         GetTree().ReloadCurrentScene();
     }
 }
+
+
+//https://docs.godotengine.org/en/stable/getting_started/step_by_step/signals.html
