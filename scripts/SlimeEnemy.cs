ba84dcfe5cd745aba4060f8faf38af200da7972a
saitakasaine <saitakasaine@gmail.com>
2024-11-19 16:55:08 +0300

    Slime Take 2

--- scripts/SlimeEnemy.cs
+++ scripts/SlimeEnemy.cs
@@ -2,11 +2,13 @@ using Godot;
 
 public partial class SlimeEnemy : Node2D
 {
-    private const int Speed = 60;
+    private float _speed = 60f;
 
-    private void _Process(float delta)
+    public void _Process(float delta)
     {
-        var position = new Vector2(Position.X * Speed * delta , Position.Y);
+        
+        var position = new Vector2(Position.X , Position.Y);
         Position = position;
+        Position += new Vector2(_speed * delta, 0f);
     }
 }
